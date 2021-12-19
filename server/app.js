249eaa013057eac37fb7b4c3dc2023663eef77d7
ms3221 <hyeongjun3221@gmail.com>
2021-12-20 01:00:09 +0900

    nft 소유권 이전 db 수정

--- server/app.js
+++ server/app.js
@@ -46,13 +46,15 @@ const input = {
   imgURI : ''
 };
 
-function inputMeta(name,external,description,chain,type,imgURI){
+function inputMeta(name,external,description,chain,type,imgURI,sale,price){
   input.name = name
   input.external_url = external
   input.description = description
   input.chain = chain
   input.type = type
   input.imgURI = imgURI
+  input.sale = sale
+  input.price = price;
 }
 
 
@@ -61,14 +63,16 @@ app.post('/create',(req,res)=>{
   //프론트에서 전달받은 Meta데이터값은 함수를 작성하여 묵어서 처리했다.
   // 그 이외 account는 따로 변수를 만들어서 받아서 처리하자.
      const account = req.body.account
-     const tokenId = req.body.tokenId
+     const tokenId = req.body.tokenId 
      inputMeta(
        req.body.name,
        req.body.external_url,
        req.body.description,
        req.body.chain,
        req.body.type,
-       req.body.imgURI
+       req.body.imgURI,
+       req.body.sale,
+       req.body.price
        )
 
        //DB에 이미 한번 저장이 되어있는 지 확인하자  -- 저장되어있으면 update, 새로운사람이면 insertOne을 진행하자.
@@ -79,7 +83,7 @@ app.post('/create',(req,res)=>{
         db.collection('NFTPOST').find({'account':account}).toArray((error,result)=>{
           if(result[0] !== undefined){
             //DB에 이미 한번 저장이 되어있는 상태면 추가해주는 쿼리를 날린다.
-            db.collection('NFTPOST').update({account: 'a'},{"$push" :{"tokenIds":{"tokenId":tokenId,'type':input.type}},
+            db.collection('NFTPOST').update({account: account},{"$push" :{"tokenIds":{"tokenId":tokenId,'type':input.type}},
           });
           //metadata저장하는 쿼리
               db.collection('Types').insertOne({type:input.type,data:{tokenId:tokenId,metadata:input}})
@@ -101,12 +105,84 @@ app.post('/create',(req,res)=>{
 //메타데이터를 보내주는 작업이 끝!! 
 app.get('/erc721/:tokenId',(req,res)=>{
      let tokenId = req.params.tokenId;
-     db.collection('NFTPOST').find({'account':'0x1231`413'}).toArray((error,result)=>{
+     console.log(tokenId);
+    
+     db.collection('Types').find({'data.tokenId':tokenId}).toArray((error,result)=>{
       console.log(result);
       if(error) console.log(error);
       if(result){
-        res.send(result);
+        res.send(result[0].data.metadata);
       }
     })
 })
 
+
+//소유권이전으로인해서 특정 값을 삭제하고 삭제 후 특정 계정에 값을 옮겨주는 작업.
+
+app.post('/buy',(req,res)=>{
+    
+ const buyer = req.body.buyer;
+ const seller = req.body.seller;
+ const tokenId = req.body.tokenId;
+ const type = req.body.type;
+
+
+
+
+//특정배열의 요소에서 삭제하는 것!! 
+// seller의 tokenId를 삭제한다.
+db.collection('NFTPOST').update( 
+  {
+      "account" : seller
+  } ,
+  {
+    "$pull" : {
+          "tokenIds":{"tokenId":tokenId} 
+    }
+  }
+)
+
+
+//buyer의 Db에 tokenId를 추가시킨다.
+if(buyer !== undefined){
+  db.collection('NFTPOST').find({'account':buyer}).toArray((error,result)=>{
+    if(result[0] !== undefined){
+      //DB에 이미 한번 저장이 되어있는 상태면 추가해주는 쿼리를 날린다.
+      db.collection('NFTPOST').update({account: buyer},{"$push" :{"tokenIds":{"tokenId":tokenId,'type':type}},
+    });
+    
+    }else{
+       //처음이라면 저장해주자.
+          db.collection('NFTPOST').insertOne({account:buyer,tokenIds:[{tokenId:tokenId,type:type}]},(error,result)=>{
+           console.log(result);
+  });
+    
+    }
+  })
+ }
+
+// db.collection('NFTPOST').update( 
+//   {
+//       "tokenIds.tokenId" : '1'
+//   } ,
+//   {
+//     "$set" : {
+//           "tokenIds.$.tokenId":"5" 
+//     }
+//   }
+//   ,(err,result)=>{
+//     console.log(result);
+//   })
+
+
+
+
+//  db.collection('NFTPOST').find({tokenIds:{$elemMatch:{'tokenId':'5','type':'c'}}}).toArray((error,result)=>{
+//   console.log(result);
+//   if(error) console.log(error);
+//   if(result){
+//     res.send(result);
+//   }
+// })
+
+})
